68379339414b59cfcafb762b9cab45e2ca707ea3
nexspro <nexspro@ik.me>
2025-08-24 00:59:15 +0200

    feat(chapter-1): add first Ruby exercise with fixes

---
+++ Ruby/ProgrammingRuby_Pickaxe/Chapter 1: First Steps/Exercice.rb
@@ -0,0 +1,14 @@
+puts "Hello, coding world, from Nexs!"
+
+def sum(number1, number2)
+  number1 + number2
+end
+
+puts sum(6, 8)
+puts sum("Leo", "Cat")
+
+puts "Hello, Its Me"
+puts "The time is now #{Time.now}"
+
+puts "hello".strip 
+puts "\tgoodbye\r\n".strip
